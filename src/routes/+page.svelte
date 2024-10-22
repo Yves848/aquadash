74152cdd5a90ae0ad902393fbeac49e574c01068
yves848 <yves.godart@gmail.com>
2024-10-22 14:49:41 +0200

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {goto, beforeNavigate, afterNavigate} from "$app/navigation";
+  const handleClick = () => {
+    console.log('Button was clicked');
+    goto('/products');
+  }
+
+  beforeNavigate((navigation) => {
+    console.log('beforeNavigate', navigation);
+  });
+  afterNavigate((navigation) => {
+    console.log('afterNavigate', navigation);
+  });
+</script>
+<h1>Welcome Home!</h1>
+
+<a href="/blog" class="">Blog</a>
+<a href="/products" class="">Products</a>
+
+<button on:click={handleClick}>Place Order</button>
