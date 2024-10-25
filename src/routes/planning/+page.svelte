189262cbfc39e6d54310493ee02f0a022d7b8fdf
yves848 <yves.godart@gmail.com>
2024-10-25 22:04:28 +0200

    WIP

--- src/routes/planning/+page.svelte
+++ src/routes/planning/+page.svelte
@@ -10,16 +10,16 @@
 
 <h1 class="text-2xl underline w-full bg-slate-500 rounded-md p-2 mb-4">Planning</h1>
 <div class="table w-full">
-  <div class="table-header-group">
-    <div class="table-row">
-      <div class="table-cell text-left">Jour</div>
-      <div class="table-cell text-center">Debut</div>
-      <div class="table-cell text-center">Fin</div>
+  <div class="table-header-group mb-8">
+    <div class="table-row  bg-slate-400 h-12 text-xl shadow-xl p-4">
+      <div class="table-cell align-middle text-left">Jour</div>
+      <div class="table-cell align-middle text-center">Debut</div>
+      <div class="table-cell align-middle text-center">Fin</div>
     </div>
   </div>
   <div class="table-row-group">
     {#each planning as item, i}
-    <div class="table-row">
+    <div class="table-row pt-8">
       <div class="table-cell text-left">{Dow[i]}</div>
       <div class="table-cell text-center">{item[1].start}</div>
       <div class="table-cell text-center">{item[1].end}</div>
