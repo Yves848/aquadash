bd025e98d51ac6a8c466ad0eae230a2d8bbbc66c
yves848 <yves.godart@gmail.com>
2024-10-25 16:58:01 +0200

    WIP

--- src/routes/planning/+page.server.ts
+++ src/routes/planning/+page.server.ts
@@ -4,7 +4,7 @@ import { json } from "@sveltejs/kit";
 
 export const load  = async () => {  
   const data = await plannings.find({}).sort({joursemaine : 1, heure : 1}).toArray();
-  console.log(data);
+  // console.log(data);
   return {
       plannings: JSON.parse(JSON.stringify(data))
   };
